ef9e82fbe2fe41b65ac81fa2e1ed28729858049c
alexabruck <5918060+alexabruck@users.noreply.github.com>
2020-10-02 10:42:43 +0200

    Remove duplication of wrapRootElement

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,43 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-const React = require("react")
-const EntityIFrame = require("./src/components/EntityIFrame").default
+const gatsbyBrowserHooks = require("./gatsby-browser.js")
+const wrapRootElement = gatsbyBrowserHooks.wrapRootElement
 
-exports.wrapRootElement = ({ element }) => {
-  return (
-    <>
-      <div
-        style={{
-          backgroundColor: "black",
-          position: "absolute",
-          height: "100vh",
-          width: "100%",
-        }}
-      ></div>
-      <div
-        style={{
-          zIndex: 1,
-          position: "absolute",
-          top: "50px",
-          left: 0,
-          right: 0,
-          bottom: 0,
-        }}
-      >
-        <EntityIFrame />
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        }}
-      >
-        {element}
-      </div>
-    </>
-  )
-}
+exports.wrapRootElement = wrapRootElement
