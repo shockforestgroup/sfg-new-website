40e56e7514be75144d86fbaf48d901111903d75b
Alexa <5918060+alexabruck@users.noreply.github.com>
2020-11-12 14:04:41 +0100

    Query articles on page

--- src/pages/essays-and-works.js
+++ src/pages/essays-and-works.js
@@ -1,98 +1,64 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/LayoutWithOverlay"
 import SEO from "../components/Seo"
 
 import "./essays-and-works.css"
 
-const essayItems = [
-  {
-    title: "Cool Essay",
-    link: "/cool-essay",
-  },
-  {
-    title: "Cool Essay",
-    link: "/cool-essay",
-  },
-  {
-    title: "Cool Essay",
-    link: "/cool-essay",
-  },
-  {
-    title: "Cool Essay",
-    link: "/cool-essay",
-  },
-  {
-    title: "Cool Essay",
-    link: "/cool-essay",
-  },
-]
-
-const workItems = [
-  {
-    title: "Cool Work",
-    link: "/cool-work",
-  },
-  {
-    title: "Cool Work",
-    link: "/cool-work",
-  },
-  {
-    title: "Cool Work",
-    link: "/cool-work",
-  },
-  {
-    title: "Cool Work",
-    link: "/cool-work",
-  },
-  {
-    title: "Cool Work",
-    link: "/cool-work",
-  },
-]
-
-const EssaysAndWorks = () => (
-  <Layout>
-    <SEO title="Essays & Works" />
-    <div style={{ display: "flex" }}>
-      <div style={{ flexGrow: 1 }}>
-        <h2 className="headline-2">Essays</h2>
-        <ul className="foobar">
-          {essayItems.map(el => (
-            <li className="essays-and-works__item">
-              <Link className="essays-and-works__item__link" to={el.link}>
-                {el.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
+const EssaysAndWorks = ({ data }) => {
+  const items = data.allMarkdownRemark.nodes.map(({ frontmatter }) => ({
+    title: frontmatter.title,
+    date: frontmatter.date,
+    author: frontmatter.author,
+    link: "",
+  }))
+  return (
+    <Layout>
+      <SEO title="Essays & Works" />
+      <div style={{ display: "flex" }}>
+        <div style={{ flexGrow: 1 }}>
+          <h2 className="headline-2">Essays</h2>
+          <ul className="foobar">
+            {items.map(el => (
+              <li className="essays-and-works__item">
+                <Link className="essays-and-works__item__link" to={el.link}>
+                  {el.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div style={{ flexGrow: 1 }}>
+          <h2 className="headline-2">Works</h2>
+          <ul className="foobar">
+            {items.map(el => (
+              <li className="essays-and-works__item">
+                <Link className="essays-and-works__item__link" to={el.link}>
+                  {el.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div style={{ flexGrow: 1 }}>
-        <ul className="foobar">
-          {essayItems.map(el => (
-            <li className="essays-and-works__item">
-              <Link className="essays-and-works__item__link" to={el.link}>
-                {el.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div style={{ flexGrow: 1 }}>
-        <h2 className="headline-2">Works</h2>
-        <ul className="foobar">
-          {workItems.map(el => (
-            <li className="essays-and-works__item">
-              <Link className="essays-and-works__item__link" to={el.link}>
-                {el.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default EssaysAndWorks
+
+export const query = graphql`
+  query PageQuery {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          date
+          author
+        }
+      }
+    }
+  }
+`
