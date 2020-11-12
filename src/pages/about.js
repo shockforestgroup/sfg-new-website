ece55cb160fcde2d0de8cb9f29d821ce93b11561
Alexa <5918060+alexabruck@users.noreply.github.com>
2020-11-12 15:55:13 +0100

    Change schema of about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,32 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/LayoutWithOverlay"
 import SEO from "../components/Seo"
 
-const About = () => (
-  <Layout>
-    <SEO title="Cool Essay" />
-    <h1 className="headline-1">About</h1>
-    About about about!
-  </Layout>
-)
+const About = ({ data }) => {
+  const aboutData = {
+    title: data.markdownRemark.frontmatter.title,
+    html: data.markdownRemark.html,
+  }
+  return (
+    <Layout>
+      <SEO title="About" />
+      <h1 className="headline-1">{aboutData.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: aboutData.html }} />
+    </Layout>
+  )
+}
 
 export default About
+
+export const query = graphql`
+  query MyQuery {
+    markdownRemark(fields: { slug: { eq: "/pages/about/" } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
