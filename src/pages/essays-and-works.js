df440c8c346f77eb7328837711916278f03c9703
Alexa <5918060+alexabruck@users.noreply.github.com>
2020-11-12 14:27:28 +0100

    Query essays and works seperately

--- src/pages/essays-and-works.js
+++ src/pages/essays-and-works.js
@@ -8,7 +8,13 @@ import SEO from "../components/Seo"
 import "./essays-and-works.css"
 
 const EssaysAndWorks = ({ data }) => {
-  const items = data.allMarkdownRemark.nodes.map(({ frontmatter }) => ({
+  const essayItems = data.essays.nodes.map(({ frontmatter }) => ({
+    title: frontmatter.title,
+    date: frontmatter.date,
+    author: frontmatter.author,
+    link: "",
+  }))
+  const workItems = data.works.nodes.map(({ frontmatter }) => ({
     title: frontmatter.title,
     date: frontmatter.date,
     author: frontmatter.author,
@@ -21,7 +27,7 @@ const EssaysAndWorks = ({ data }) => {
         <div style={{ flexGrow: 1 }}>
           <h2 className="headline-2">Essays</h2>
           <ul className="foobar">
-            {items.map(el => (
+            {essayItems.map(el => (
               <li className="essays-and-works__item">
                 <Link className="essays-and-works__item__link" to={el.link}>
                   {el.title}
@@ -33,7 +39,7 @@ const EssaysAndWorks = ({ data }) => {
         <div style={{ flexGrow: 1 }}>
           <h2 className="headline-2">Works</h2>
           <ul className="foobar">
-            {items.map(el => (
+            {workItems.map(el => (
               <li className="essays-and-works__item">
                 <Link className="essays-and-works__item__link" to={el.link}>
                   {el.title}
@@ -51,7 +57,20 @@ export default EssaysAndWorks
 
 export const query = graphql`
   query PageQuery {
-    allMarkdownRemark {
+    essays: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/(essays)/" } }
+    ) {
+      nodes {
+        frontmatter {
+          title
+          date
+          author
+        }
+      }
+    }
+    works: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/(works)/" } }
+    ) {
       nodes {
         frontmatter {
           title
