0ffc17884f86a1b8b56a34e5bfbdbdbc3c1e5d9f
Alexa <5918060+alexabruck@users.noreply.github.com>
2020-11-20 14:05:50 +0100

    Add test data

---
+++ test/data/essayItems.js
@@ -0,0 +1,16 @@
+const essayItems = [
+  {
+    title: "The figuring of buoys and swell 2 a bit longer title asdf asdf 111",
+    date: new Date(),
+    author: "Alexa",
+    link: "/foobar",
+  },
+  {
+    title: "The figuring of buoys and swell 2 a bit longer title asdf asdf ",
+    date: new Date(),
+    author: "Alexa",
+    link: "/foobar",
+  },
+]
+
+export default essayItems
---
+++ test/data/workItems.js
@@ -0,0 +1,22 @@
+const workItems = [
+  {
+    title: "Work: The figuring of buoys and swell 1 ",
+    date: new Date(),
+    author: "Alexa",
+    link: "/foobar",
+  },
+  {
+    title: "Work: The figuring of buoys and swell 2 ",
+    date: new Date(),
+    author: "Alexa",
+    link: "/foobar",
+  },
+  {
+    title: "Work: The figuring of buoys and swell 3",
+    date: new Date(),
+    author: "Alexa",
+    link: "/foobar",
+  },
+]
+
+export default workItems
