a46a49180932133f79e51f1c9b686c5ed9122e3a
Alexa <5918060+alexabruck@users.noreply.github.com>
2020-12-18 14:36:04 +0100

    Clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 
-const About = () => (
+export default () => (
   <Layout>
     <SEO title="Home" />
   </Layout>
 )
-
-export default About
