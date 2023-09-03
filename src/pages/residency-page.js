c2656112ca6b569828c1f1013cacdf6d254f313c
jelkro <jelgerkroese@gmail.com>
2023-09-03 17:37:38 +0200

    Add null check in case there is no description data.

--- src/pages/residency-page.js
+++ src/pages/residency-page.js
@@ -10,7 +10,7 @@ import { enableBodyScroll } from 'body-scroll-lock';
 import "./residency-page.css"
 
 const ResidencyPage = ({ data }) => {
-  const description = data.description.html
+  const description = (data.description != null ? data.description.html : "no description found"); 
   const essayItems = data.essays.nodes.map(({ frontmatter, fields }) => ({
     title: frontmatter.title,
     date: frontmatter.date,
