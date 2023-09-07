e1a75d42812d10427d6af5221c14885be864e51b
jelkro <jelgerkroese@gmail.com>
2023-09-07 17:04:40 +0200

    removed unnecessary elements from page.

--- src/templates/residency-page.js
+++ src/templates/residency-page.js
@@ -9,7 +9,6 @@ import { enableBodyScroll } from 'body-scroll-lock';
 import "./residency-page.css"
 
 const ResidencyPage = ({ data }) => {
-  // const description = (data.description != null ? data.description.html : "no description found");
   const description = data?.description.html
   const essayItems = data?.essays.nodes.map(({ frontmatter, fields }) => ({
     title: frontmatter.title,
@@ -39,29 +38,22 @@ const ResidencyPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Research Location" />
-      <div
-      style={{ marginBottom: "100px" }}
-      dangerouslySetInnerHTML={{ __html: description }}
-      />
-      <h2 className="headline-1">Works</h2>
-          <ul className="residency-page__list">
-            {workItems.map(el => (
-              <li className="residency-page__item">
-                  <div className="hide-child">
-                    <Link to={el.link}>
-                      <Img 
+      <ul className="residency-page__list">
+        {workItems.map(el => (
+          <li className="residency-page__item">
+            <Link to={el.link}>
+              {/* <Img 
                         fluid={el.coverImage.childImageSharp.fluid}
                         alt="cover img"
                         title="test"
-                      />
-                    </Link>
-                    <p className="child top-0 left-0 right-0 bottom-0 flex items-center justify-center f3 white bg-black-60">
-                      {el.title}
-                    </p>                    
-                  </div>
-              </li>
-            ))}
-          </ul>
+                      /> */}
+              <p className="top-0 left-0 right-0 bottom-0 flex items-center justify-left white">
+                {el.title}
+              </p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
