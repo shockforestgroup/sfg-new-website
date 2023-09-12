d8e4d9d976fae2f942aa527fd905ed8ed5092138
jelkro <jelgerkroese@gmail.com>
2023-09-12 17:44:36 +0200

    Added code to expand and collapse subpage content. removed old code to render images.

--- src/templates/residency-page.js
+++ src/templates/residency-page.js
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
-import Img from "gatsby-image"
+import { navigate } from 'gatsby';
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
@@ -8,7 +8,7 @@ import { enableBodyScroll } from 'body-scroll-lock';
 
 import "./residency-page.css"
 
-const ResidencyPage = ({ data }) => {
+const ResidencyPage = ({ data }, location) => {
   const description = data?.description?.html
   const essayItems = data?.essays?.nodes.map(({ frontmatter, fields }) => ({
     title: frontmatter.title,
@@ -26,6 +26,23 @@ const ResidencyPage = ({ data }) => {
     link: fields.slug,
   }))
 
+  //handling expand collapse and url setting
+  const [expandedId, setExpandedId] = useState(null);
+
+  useEffect(() => {
+    if (location.state && location.state.subPageId) {
+      setExpandedId(location.state.subPageId);
+    }
+  }, [location]);
+
+  const handleLinkClick = (id) => {
+    setExpandedId(id);
+    navigate(`/${id}`, {
+      state: { subPageId: id },
+    });
+  };
+
+  //Bodyscroll
   React.useEffect(() => {
     gatsbyEnableBodyScroll();
   }, []);
@@ -46,16 +63,16 @@ const ResidencyPage = ({ data }) => {
       <ul className="residency-page__list">
         {workItems.map((el, index) => (
           <li className="residency-page__item" key={index}>
-            <Link to={el.link}>
-              {/* <Img 
-                        fluid={el.coverImage.childImageSharp.fluid}
-                        alt="cover img"
-                        title="test"
-                      /> */}
+            <Link
+              to={el.link}
+              state={{ subPageId: index }}
+              onClick={() => handleLinkClick(index)}
+            >
               <p className="top-0 left-0 right-0 bottom-0 items-center justify-left white">
                 {String(workItems.length - index).padStart(2, '0')}. {el.title}
               </p>
             </Link>
+            {expandedId === index && <div>{`Content for page ${index}`}</div>}
           </li>
         ))}
       </ul>
