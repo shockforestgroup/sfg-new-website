481ce5f8428c366faad45c5be945739b33fdfb90
jelkro <jelgerkroese@gmail.com>
2023-10-04 16:09:01 +0200

    disable body scroll on Entity page.

--- src/pages/the-entity.js
+++ src/pages/the-entity.js
@@ -1,26 +1,43 @@
 import React from "react"
 
+import { disableBodyScroll } from 'body-scroll-lock';
 import EntityIFrame from "../components/EntityIFrame"
 import SEO from "../components/Seo"
 
-const TheEntity = () => (
-  <>
-    <SEO title="The Entity" />
-    <div
-      style={{
-        zIndex: 1,
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <EntityIFrame />
-    </div>
-  </>
-)
+
+
+
+const TheEntity = () => {
+
+  React.useEffect(() => {
+    gatsbyDisableBodyScroll();
+  }, []);
+
+  const gatsbyDisableBodyScroll = (event) => {
+    document.getElementsByTagName("html")[0].style = "";
+    disableBodyScroll(document.body);
+  };
+
+  return (
+    <>
+      <SEO title="The Entity" />
+      <div
+        style={{
+          zIndex: 1,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <EntityIFrame />
+      </div>
+    </>
+  )
+}
+
 
 export default TheEntity
